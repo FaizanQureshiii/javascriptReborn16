559248143395c8a2dec905d440f6da1d1e8a3c90
MuhammadFaizan121 <140752231+MuhammadFaizan121@users.noreply.github.com>
2024-10-17 21:54:55 +0500

    thisKeyword

---
+++ jsbasic/thiskeyword.js
@@ -0,0 +1,36 @@
+const login ={
+    username:'faizi',
+    password:'12345',
+
+    welcomemessage:function(){
+        console.log(`${this.username},welcome to the network`);
+    }
+    
+}
+
+//this keyword refers to the current object 
+//fe if we use this keyword in the browser then we will have windows as current object
+
+
+
+
+//this keyword cant be used in functions
+//
+//if we use arrow unction we dont have to use function keyword 
+//like syntsx is ()= > {}
+//
+//const addtwonumber=() = >num1+num2;
+//
+//implicit return when we dont use return keyword and explecit return is when we use return keyword
+
+
+console.log(this);
+login.welcomemessage();
+
+login.username='fahad';
+
+login.welcomemessage();
+
+const addsum= (num1 , num2 )=>num1+num2;
+console.log(addsum(1,2));
+
