9ea08315d43d4269886eb2aab36d92682e93b4af
MuhammadFaizan121 <140752231+MuhammadFaizan121@users.noreply.github.com>
2024-10-21 00:26:26 +0500

    iife

---
+++ jsbasic/IIFE.js
@@ -0,0 +1,20 @@
+
+(function chai(){
+    console.log('network connected');
+
+})();
+
+//parameter
+
+((name) =>{
+    console.log(`hello connect me ${name}`);
+})('faizan');
+
+
+
+//iife
+//()()
+
+
+//iife global scope ke pollution se problem hoti hai kayi bar toh us global scope ke variables hain uskay pollution ko hatanay ke liye we use iife
+
