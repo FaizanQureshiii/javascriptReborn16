cae903f80bfede0493c4d148a654842dbb0b9639
faizanqureshii <muhammadfaizanqureshiii1@gmail.com>
2024-11-01 02:18:57 +0500

    To-Do List

---
+++ js projects/todo list/main.js
@@ -0,0 +1,44 @@
+const input=document.getElementById("input-task");
+const ul=document.querySelector(".listss");
+const checkbox=document.querySelectorAll(".checkbox");
+
+input.addEventListener("keyup",function(e){
+    
+    e.preventDefault();
+  
+ 
+    if(e.key=="Enter" && input.value==""){
+        alert("Please enter a task");
+        return false;
+    }
+    else if(e.key=="Enter"){ 
+
+   
+    const li=document.createElement("li");
+    li.className="list-item";
+    li.appendChild(document.createTextNode(input.value));
+    const check=document.createElement("input");
+    check.type="checkbox";
+    check.className="checkbox";
+    li.appendChild(check);
+    ul.appendChild(li);
+
+    input.value="";
+
+        check.addEventListener("click",function(){
+            if(check.checked){
+                li.style.textDecoration="line-through";
+                setTimeout(() => {
+                    ul.removeChild(li);
+                }, 5000);
+
+            }
+            else if(!check.checked){
+                li.style.textDecoration="none";
+            }
+        
+        })
+   
+    }
+
+})
